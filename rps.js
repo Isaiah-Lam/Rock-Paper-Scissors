c716b9198ee2cda133491a1502b1db05b24703d0
Isaiah-Lam <isaiah@lamison.net>
2021-11-09 15:54:24 -0500

    functional game

---
+++ rps.js
@@ -0,0 +1,70 @@
+var choices = ['rock', 'paper', 'scissors'];
+playerScore = 0;
+cpuScore = 0;
+
+function checkWin(p,c) {
+    if (p == "rock") {
+        if (c == "paper") {
+            return "cpu";
+        }
+        else if (c == "scissors") {
+            return "player";
+        }
+        else {
+            return "tie";
+        }
+    }
+    else if (p == "paper") {
+        if (c == "paper") {
+            return "tie";
+        }
+        else if (c == "scissors") {
+            return "cpu";
+        }
+        else {
+            return "player";
+        }
+    }
+    else if (p == "scissors") {
+        if (c == "paper") {
+            return "player";
+        }
+        else if (c == "scissors") {
+            return "tie";
+        }
+        else {
+            return "cpu";
+        }
+    }
+}
+
+function game(player) {
+    
+    let cpu = choices[Math.floor(Math.random()*3)];
+    console.log(player + " v " + cpu);
+    let winner = checkWin(player, cpu);
+
+    if (winner == "tie") {
+        // display tie message
+    }
+
+    else {
+        if (winner == "player") {
+            playerScore++;
+            document.getElementById('playerScore').innerHTML = playerScore;
+        }
+        else if (winner == "cpu") {
+            cpuScore++;
+            document.getElementById('cpuScore').innerHTML = cpuScore;
+        }
+
+        let total = playerScore + cpuScore;
+        let p1Width = Math.round((playerScore/total)*10000) / 100
+        let p2Width = 100 - p1Width
+        document.getElementById('p1').style.width = p1Width + "%";
+        document.getElementById('p2').style.width = p2Width + "%";
+
+    }
+    
+    
+}
